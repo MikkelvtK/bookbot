97168d9c060975d6182afad72d7ac0d8750c0d7e
MikkelvtK <mvtklooster@hotmail.com>
2024-10-17 19:05:59 +0200

    order character display

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 def main():
     path = "./books/frankenstein.txt"
     text = get_book_text(path)
-    chars = count_characters(text)
-    print(chars)
-
+    word_count = count_words(text)
+    char_count = count_characters(text)
+    display_report(word_count, char_count, path)
 
 def get_book_text(path):
     with open(path) as f:
@@ -18,12 +18,29 @@ def count_characters(text):
     result = {}
 
     for char in text.lower():
-        if char not in result:
-            result[char] = 0
+        if char.isalpha():
+            if char not in result:
+                result[char] = 0
 
-        result[char] += 1
+            result[char] += 1
 
     return result
+
+
+def sort_chars(chars):
+    return sorted(chars)
+
+
+def display_report(word_count, char_count, path):
+    print(f"----- Report of {path} -----")
+    print(f"Number of words in the book: {word_count}")
+    print()
+    display_characters(char_count)
+
+
+def display_characters(char_count):
+    for char in sort_chars(char_count):
+        print(f"The '{char}' character was found {char_count[char]} times")
 
 
 main()
